e0e425d36d318af601ac29ff2a84b0dd3fc52f6b
Aleksey Vetrov <vetrovalexey28@gmail.com>
2024-04-23 15:40:51 +0300

    исправил basePath, сделал уникальные запросы, переместил  ассерты в RestAssuredClient

---
+++ src/main/java/ananasovitch/org/library2/service/Endpoints.java
@@ -0,0 +1,11 @@
+package ananasovitch.org.library2.service;
+
+public class Endpoints {
+    public static final String BASE_URI = "http://localhost:8080";
+    public static final String BASE_PATH = "/library";
+
+    public static final String AUTHORS_SAVE = "/authors/save";
+    public static final String BOOKS_SAVE = "/books/save";
+    public static final String AUTHORS_BOOKS = "/authors/{id}/books";
+    public static final String CUSTOMERS = "/customer/customers/{filter}";
+}
